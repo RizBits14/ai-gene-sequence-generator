24fa36c547026a4e72cde6642092da3c4b97849a
RizBits14 <mohammad.rizwanul.islam12014@gmail.com>
2025-07-21 23:19:52 +0600

    creating the utility function for further use

---
+++ code-dump/utility-function.py
@@ -0,0 +1,23 @@
+def calculate_utility(gene_sequence, target_sequence, weights):
+    utility = 0
+    maximum_length = max(len(gene_sequence), len(target_sequence))
+
+    for i in range(maximum_length):
+        if i < len(gene_sequence):
+            gene_character = ord(gene_sequence[i])
+        else:
+            gene_character = 0
+        
+        if i < len(target_sequence):
+            target_character = ord(target_sequence[i])
+        else:
+            target_character = 0
+        
+        if i < len(weights):
+            weight = weights[i]
+        else:
+            weight = 1
+
+        utility -= weight * abs(gene_character - target_character)
+
+    return utility
